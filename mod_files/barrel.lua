1d4bfb3031db4f20f03d1ae31690b607f2e97905
Nico Schönerstedt <nico.schoenerstedt@conuti.de>
2024-02-14 19:06:12 +0100

    wrote barrel code again entirely from scratch

--- mod_files/barrel.lua
+++ mod_files/barrel.lua
@@ -1,100 +1,153 @@
---------------------------
--- Items                --
---------------------------
+local Barrel = {}
 
---------------------------
--- blocks               --
---------------------------
+Barrel.new = function (pos) 
+    local self = {}
+    -- private property declarations
+    local meta, inventory, getSoakingItemStack
 
-function default.get_barrel_empty_formspec(infotext)
-	return "size[8,8.5]" ..
-		"padding[1,1]" ..
-		"label[0,0.0;"..infotext.."]" ..
-		"image[2,1;1,3.35;gui_barrel_bar.png]" ..
-		"list[context;liquid;3,1;1,1;]" ..
-		"list[context;src;3,3;1,1;]" ..
-		"image[4,3;1,1;gui_barrel_arrow_bg.png]" ..
-		"list[context;dst;5,3;1,1;]" ..
-		"list[context;buk;6,3;1,1;]" ..
-		"button[5,1.2;2,0.5;test;Seal Barrel]" ..
-		"list[current_player;main;0,4.5;8,4;]" ..
-	default.get_hotbar_bg(0, 4.5)
+    -- private method declarations
+    local initialize,setInventory
+
+
+    -- constructor
+    local construct = function ()
+        meta = minetest.get_meta(pos)
+        inventory = meta:get_inventory()
+
+        if (meta:get_int("initialized") == 0) then
+            initialize()
+        end
+    end
+
+
+    -- public methods
+    self.setFormSpec = function(formspec)
+        minetest.log("action", "set formspec ...")
+        minetest.log("action", dump2("FORMSPEC: ", formspec))
+        meta:set_string("formspec", formspec)
+    end
+
+    self.getLiquidStack = function ()
+        return inventory:get_list("liquid")[1]
+    end
+
+    self.getSourceInventory = function ()
+        return inventory:get_list("src")
+    end
+
+    self.getDestinyInventory = function ()
+       return inventory:get_list("dst")
+    end
+
+    self.hasSoakingItems = function ()
+        local soakingItemStack = getSoakingItemStack()
+        if (soakingItemStack ~= nil) then
+            return true
+        end
+
+        return false
+    end
+
+    self.soak = function ()
+        local soakingItemStack, soakingItemStackIndex = getSoakingItemStack()
+        local takenSoaking = soakingItemStack:take_item(1)
+
+        local liquidStack = self.getLiquidStack()
+        local takenLiquid = liquidStack:take_item(1)
+
+        if (takenSoaking:get_count() > 0 and takenLiquid:get_count() > 0) then
+            inventory:set_stack("src", soakingItemStackIndex, soakingItemStack)
+            inventory:set_stack("liquid", 1, liquidStack)
+
+            local dst_stack = inventory:get_stack('dst', 1)
+            dst_stack:add_item("beer_test:soaked_barley")
+            inventory:set_stack('dst', 1, dst_stack)
+         
+            local bucket_stack = inventory:get_stack('liquid', 1)
+            bucket_stack:add_item("bucket:bucket_empty")
+            inventory:set_stack('liquid', 1, bucket_stack)
+        end
+    end
+
+
+    -- private methods
+    setInventory = function (name, size)
+        inventory:set_size(name, 1)
+    end
+
+    initialize = function ()
+        setInventory("liquid", 1)
+        setInventory("src", 1)
+        setInventory("dst", 1)
+        setInventory("buk", 1)
+        minetest.log("action", "initialized!")
+        self.setFormSpec(Barrel.formspecs.default("Empty Barrel"))
+        meta:set_int("initialized", 1)
+    end
+
+    getSoakingItemStack = function ()
+        local sourceInventory = self.getSourceInventory()
+        for i, itemStack in ipairs(sourceInventory) do
+            if (itemStack:get_name() == "beer_test:cracked_barley") then
+                return itemStack, i
+            end
+        end
+
+        return nil
+    end
+
+    construct()
+    return self;
 end
 
+Barrel.formspecs = {
+    default = function (infotext)
+        return table.concat({
+            "size[8,8.5]",
+            "label[0,0.0;"..infotext.."]",
+            "image[2,1;1,3.35;gui_barrel_bar.png]",
+            "list[context;liquid;3,1;1,1;]",
+            "list[context;src;3,3;1,1;]",
+            "image[4,3;1,1;gui_barrel_arrow_bg.png]",
+            "list[context;dst;5,3;1,1;]",
+            "list[context;buk;6,3;1,1;]",
+            "button[5,1.2;2,0.5;test;Seal Barrel]",
+            "list[current_player;main;0,4.5;8,4;]",
+            default.get_hotbar_bg(0, 4.5)
+        }, "")
+    end, 
+    soaking = function ()
 
-function default.get_barrel_active_formspec()
-	return "size[8,7.5]" ..
-		"image[1,0;1,3.35;gui_barrel_bar.png]" ..
-		"image[1,0;1,3.35;gui_barrel_bar_fg.png]" ..
-		"image[4,1;1,1;gui_barrel_arrow_bg.png]" ..
-		"list[current_player;main;0,3.5;8,4;]" ..
-		"list[context;main;2,0;2,3;]" ..
-		"list[context;out;5,1;1,1;]" ..
-		"label[5,0.0;Barrel]" ..
-		"button[5,2.5;2,0.5;test;Seal Barrel]" ..
-	default.get_hotbar_bg(0, 4.25)
+    end,
+    fermenting = function ()
+
+    end
+}
+
+Barrel.onConstruct = function (pos)
+    minetest.log("action", "creating barrel ...")
+    local barrel = Barrel.new(pos)
 end
 
+Barrel.onTimer = function (pos, time)
+    local barrel = Barrel.new(pos)
+    if (barrel.hasSoakingItems()) then
+        barrel.soak()
+    end
+    --[[
+        if (barrel.isSealed()) then
+            barrel.ferment()
+        end
+    ]]--
+end
 
-
-
-local function can_dig(pos, player, node)
-	local meta = minetest.get_meta(pos);
+Barrel.canDig = function (pos, player, node)
+    local meta = minetest.get_meta(pos);
 	local inv = meta:get_inventory()
 	return inv:is_empty("src") and inv:is_empty("dst")
 end
 
-local function barrel_node_timer(pos, newFull)
-	
-	local meta = minetest.get_meta(pos)
-	-- GUI elements so fromspec and GUI
-	local formspec
-	local infotext
-
-	--[[
-	
-	--local meta = minetest.get_meta(pos)
-    local inv = meta:get_inventory()
-    local srclist, liquidlist 
-
-    srclist = inv:get_list("src")
-	liquidlist = inv:get_list("liquid")
- 	
- 	infotext = ("Barrel "..srclist.."")
-
-    -- Check if there is fuel
-    if liquidlist == "bucket:bucket_water" then
-		for i, itemstack in ipairs(srclist) do
-            if itemstack == "default:apple" then
-                -- Remove the apple from src
-                itemstack:take_item()
-                inv:set_stack('src', i, itemstack)
-
-                -- Add a soaked barley apple to dst
-                inv:add_item('dst', "beer_test:soaked_barley")
-                inv:set_stack('dst', 1, dst_stack)
-
-     
-            end
-        end
-    end
-	]]--
-
-
-	if newFull == 0 then
-		infotext = ("Empty Barrel")
-		formspec = default.get_barrel_empty_formspec(infotext)
-	else
-		infotext = ("Barrel "..newFull.."% Full")
-		formspec = default.get_barrel_active_formspec()
-	end
-
-	meta:set_string("formspec", formspec)
-	meta:set_string("infotext", infotext)
-end
-
-
-minetest.register_node("beer_test:barrel", {
+minetest.register_node("beer_test:barrel",{
     description = "Empty Barrel",
 	drawtype = "nodebox",
     tiles = {"beer_test_barrel_top.png", "beer_test_barrel_top.png", "beer_test_barrel_side_2.png",
@@ -103,34 +156,20 @@ minetest.register_node("beer_test:barrel", {
     paramtype2 = "facedir",
     groups = {cracky=2},
     sounds = default.node_sound_wood_defaults(),
-    -- node stuff 
-    can_dig = can_dig,
-	on_timer =  barrel_node_timer,
-    on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		-- set barrel inv size
-		local inv = meta:get_inventory()
-		inv:set_size("liquid", 1)
-		inv:set_size("src", 1)
-		inv:set_size("dst", 1)
-		inv:set_size("buk", 1)
-		newFull = meta:get_int("full")
-
-		barrel_node_timer(pos, newFull, node) 
-
-	end,
-
-	--[[	on_punch = function(pos, node, puncher) 
-		local meta = minetest.get_meta(pos)
-		local tool = puncher:get_wielded_item():get_name()
-		if tool and tool == "bucket:bucket_water" then
-			puncher:get_inventory():remove_item("main", ItemStack("bucket:bucket_water"))
-			puncher:get_inventory():add_item("main", ItemStack("bucket:bucket_empty"))
-			meta:set_int("full",100);
-
-		end
-	end,
-	]]
+    can_dig = Barrel.canDig,
+	on_timer =  Barrel.onTimer,
+    on_construct = Barrel.onConstruct,
+    on_metadata_inventory_move = function(pos)
+        minetest.get_node_timer(pos):start(1.0)
+    end,
+    on_metadata_inventory_put = function(pos)
+        -- start timer function, it will sort out whether furnace can burn or not.
+        minetest.get_node_timer(pos):start(1.0)
+    end,
+    on_metadata_inventory_take = function(pos)
+        -- check whether the furnace is empty or not.
+        minetest.get_node_timer(pos):start(1.0)
+    end,
     node_box = {
         type = "fixed",
         fixed = {
@@ -139,7 +178,6 @@ minetest.register_node("beer_test:barrel", {
             {-0.5, -0.5, -0.5, -0.35, 0.5, 0.5}, -- side l
             {0.35, -0.5, -0.5, 0.5, 0.5, 0.5},  -- side r
             {-0.5, -0.5, -0.35, 0.5, 0.5, -0.5}, -- frount
-             
         },
     },
     selection_box = {
@@ -147,48 +185,5 @@ minetest.register_node("beer_test:barrel", {
         fixed = {
             {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
         },
-    }, 
-})
-
--------------------
-
-minetest.register_abm({
-    nodenames = {"beer_test:barrel"},
-    interval = 1.0, -- Run every second
-    chance = 1, -- Always run for all nodes
-    action = function(pos, node, active_object_count, active_object_count_wider)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        local src_list = inv:get_list('src')
-        local liquid_list = inv:get_list('liquid')
-
-        -- Check if there is fuel
-        if liquid_list[1]:get_name() == "bucket:bucket_water" then
-            for i, itemstack in ipairs(src_list) do
-                if itemstack:get_name() == "beer_test:cracked_barley" then
-                    -- Remove the apple from src
-                    itemstack:take_item()
-                    inv:set_stack('src', i, itemstack)
-
-                    local liquid_stack = liquid_list[1]
-                    liquid_stack:set_count(liquid_stack:get_count() - 1)
-                    inv:set_stack('liquid', 1, liquid_stack)
-
-
-                    -- add soaked barley and give back bukket 
-            
-                    local dst_stack = inv:get_stack('dst', 1)
-                    dst_stack:add_item("beer_test:soaked_barley")
-                    inv:set_stack('dst', 1, dst_stack)
-         
-                    local bucket_stack = inv:get_stack('liquid', 1)
-                    bucket_stack:add_item("bucket:bucket_empty")
-                    inv:set_stack('liquid', 1, bucket_stack)
-               
-
-
-                end
-            end
-        end
-    end,
+    },
 })
