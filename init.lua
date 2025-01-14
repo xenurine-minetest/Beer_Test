82f3a0a0169aba4eda46a64dca542bf1644daf88
xenurine <sean.emmett4@gmail.com>
2025-01-14 22:06:04 +0000

    updated mesh for barrels

--- init.lua
+++ init.lua
@@ -45,6 +45,12 @@ dofile(minetest.get_modpath("beer_test").."/mod_files/other_mods.lua")
 print("Beer_test: Loading 'other_mods.lua'")
 dofile(minetest.get_modpath("beer_test").."/mod_files/growing_rope.lua")
 
+
+-- new file for the barrel --
+
+print("Beer_test: Loading 'barrel.lua'")
+dofile(minetest.get_modpath("beer_test").."/mod_files/barrel.lua")
+
 print("")
 print("###########################################################")
 print("Beer_Test has loaded successfully ")	
---
+++ mod_files/barrel.lua
@@ -0,0 +1,96 @@
+-----------------
+-- beer barrle --
+-----------------
+
+minetest.register_node("beer_test:barrel", {
+    description = "Barrel",
+    drawtype = "nodebox",
+    tiles = {"beer_test_barrel_top.png", "beer_test_barrel_top.png", "beer_test_barrel_side.png",
+    "beer_test_barrel_side.png", "beer_test_barrel_side.png", "beer_test_barrel_side.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2},
+    sounds = default.node_sound_wood_defaults(),
+    use_texture_alpha = "blend",
+	on_punch = function(pos, node, puncher)
+		local tool = puncher:get_wielded_item():get_name()
+		if tool and tool == "beer_test:mixed_beer_grain" then
+			node.name = "beer_test:barrel_mixed_beer_grain"
+			minetest.env:set_node(pos, node)
+			puncher:get_inventory():remove_item("main", ItemStack("beer_test:mixed_beer_grain"))
+		end
+			
+		local tool = puncher:get_wielded_item():get_name()
+		if tool and tool == "beer_test:mixed_ale_grain" then
+			node.name = "beer_test:barrel_mixed_ale_grain"
+			minetest.env:set_node(pos, node)
+			puncher:get_inventory():remove_item("main", ItemStack("beer_test:mixed_ale_grain"))
+			
+		end
+		
+		local tool = puncher:get_wielded_item():get_name()
+		if tool and tool == "beer_test:mixed_mead_grain" then
+			node.name = "beer_test:barrel_mixed_mead_grain"
+			minetest.env:set_node(pos, node)
+			puncher:get_inventory():remove_item("main", ItemStack("beer_test:mixed_mead_grain"))
+			
+		end
+	end,
+    on_construct = function(pos)
+        local meta = minetest.get_meta(pos)
+        meta:set_string("infotext", "Barrel")
+    end,
+    node_box = {
+        type = "fixed",
+        fixed = {
+			{-0.375, -0.5, 0.3125, 0.4375, 0.5, 0.4375}, -- NodeBox1
+			{-0.4375, -0.5, -0.375, -0.3125, 0.5, 0.4375}, -- NodeBox2
+			{-0.4375, -0.5, -0.4375, 0.375, 0.5, -0.3125}, -- NodeBox3
+			{0.3125, -0.5, -0.4375, 0.4375, 0.5, 0.375}, -- NodeBox4
+			{-0.375, -0.4375, -0.375, 0.375, 0.4375, 0.375}, -- NodeBox5
+		}
+    },
+    selection_box = {
+        type = "fixed",
+        fixed = {
+            {-0.4375, -0.5, -0.4375, 0.4375, 0.5, 0.4375}, -- NodeBox1
+        },
+    }, 
+		
+})
+
+minetest.register_node("beer_test:brewing_barrel", {
+	description = "Brewing Barrel",
+	drawtype = "nodebox",
+    tiles = {"default_wood.png^beer_test_barrel_side.png", "default_wood.png^beer_test_barrel_side.png", "default_wood.png^beer_test_brewing_barrel_side.png",
+    "default_wood.png^beer_test_brewing_barrel_side.png", "default_wood.png^beer_test_brewing_barrel_front.png", "default_wood.png^beer_test_brewing_barrel_front.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2},
+    sounds = default.node_sound_wood_defaults(),
+    use_texture_alpha = "blend",
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.375, -0.5, 0.4375, -0.25, 0.5}, -- NodeBox2
+			{-0.4375, -0.375, -0.5, -0.3125, 0.4375, 0.5}, -- NodeBox3
+			{-0.4375, 0.375, -0.5, 0.375, 0.5, 0.5}, -- NodeBox4
+			{0.3125, -0.3125, -0.5, 0.4375, 0.5, 0.5}, -- NodeBox5
+			{-0.375, -0.3125, -0.4375, 0.4375, 0.4375, 0.4375}, -- NodeBox6
+			{-0.5, -0.5, 0.3125, 0.5, -0.375, 0.4375}, -- NodeBox8
+			{-0.5, -0.5, -0.4375, 0.5, -0.375, -0.3125}, -- NodeBox9
+		}
+	},
+	selection_box = {
+        type = "fixed",
+        fixed = {
+           -- {-0.437, -0.5, -0.5, 0.4375, 0.5, 0.5}, -- NodeBox1
+            {-0.437, -0.375, -0.5, 0.4375, 0.5, 0.5}, -- NodeBox1
+            {-0.5, -0.5, 0.3125, 0.5, -0.375, 0.4375}, -- NodeBox8
+			{-0.5, -0.5, -0.4375, 0.5, -0.375, -0.3125}, -- NodeBox9
+        },
+    }, 
+})
+print("Beer_test: barrel.lua            [ok]")
